be3880bcb3ff9a9d30010e0a83f7fe4d990b86fd
Hasindu-Dilshan <hasindudilshan10@gmail.com>
2024-03-25 18:14:13 +0530

    Fix getDriverById

--- src/controllers/driver.controller.js
+++ src/controllers/driver.controller.js
@@ -52,11 +52,9 @@ exports.getDriverById = async (req, res, next) => {
   const id = req.params.id;
 
   const driver = await Driver.findById(id).catch((error) => {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).then((data) => {
-      return res.status(StatusCodes.OK).json({
-        success: true,
-        data,
-      });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: true,
+      data,
     });
   });
 
@@ -69,7 +67,7 @@ exports.getDriverById = async (req, res, next) => {
 
   res.status(StatusCodes.OK).json({
     success: true,
-    data,
+    driver,
   });
 };
 
