d84c581cbba47c988dd59267f81e963873558894
Hasindu-Dilshan <hasindudilshan10@gmail.com>
2024-04-07 22:16:07 +0530

    Change login endpoint

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -3,7 +3,7 @@ const { adminLogin, adminCreate } = require('../../controllers/auth.controller')
 
 const router = express.Router();
 
-router.route('/admin/login').post(adminLogin);
+router.route('/login').post(adminLogin);
 router.route('/admin/create').post(adminCreate);
 
 module.exports = router;
